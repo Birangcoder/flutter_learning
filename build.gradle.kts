d3214005b85e3c643215376efda15f6ff0de3be6
Birangcoder <113354710+Birangcoder@users.noreply.github.com>
2026-02-10 20:40:33 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // Google Services plugin (DO NOT apply here)
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
+// Optional: custom build directory (Flutter default tweak)
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    project.layout.buildDirectory.value(newBuildDir.dir(project.name))
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
